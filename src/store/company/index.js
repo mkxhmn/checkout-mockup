d34fa8322acc6f729e5b283fe100e01060aded41
Farid <fnikman@gmail.com>
2021-06-10 00:54:08 +0800

    feat: company store, discount rules

---
+++ src/store/company/index.js
@@ -0,0 +1,45 @@
+import { action } from 'easy-peasy';
+
+export default {
+  companies: [
+    {
+      name: 'UEM Sunrise',
+      discounts: {
+        standard: {
+          type: 'deal',
+          rules: { amount: 3, to: 2 },
+        },
+      },
+    },
+    {
+      name: 'Sime Darby Property Bhd',
+      discounts: {
+        featured: {
+          type: 'drop',
+          rules: { amount: 1, to: 299.99 },
+        },
+      },
+    },
+    {
+      name: 'Mah Sing Group',
+      discounts: {
+        standard: {
+          type: 'deal',
+          rules: { amount: 5, to: 4 },
+        },
+        featured: {
+          type: 'drop',
+          rules: { amount: 4, to: 299.99 },
+        },
+        premium: {
+          type: 'drop',
+          rules: { amount: 3, to: 389.99 },
+        },
+      },
+    },
+  ],
+
+  setCompanies: action((state, payload = []) => {
+    state.companies = payload;
+  }),
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, persist } from 'easy-peasy';
 import cart from './cart';
 import products from './products';
+import company from './company';
 
 export default createStore(
   persist(
     {
       cart,
       products,
+      company,
     },
     {
       storage: 'localStorage',
