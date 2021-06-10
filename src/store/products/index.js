9ef96922ea06355ad6a77c38b2dfb37419e5e324
Farid <fnikman@gmail.com>
2021-06-10 10:12:58 +0800

    feat: implement description in products

--- src/store/products/index.js
+++ src/store/products/index.js
@@ -1,5 +1,20 @@
 export default {
-  standard: { name: 'ad', price: 269.99 },
-  featured: { name: 'ad', price: 322.99 },
-  premium: { name: 'ad', price: 394.99 },
+  standard: {
+    name: 'ad',
+    price: 269.99,
+    description: 'Offers the most basic level of advertisement',
+  },
+  featured: {
+    name: 'ad',
+    price: 322.99,
+    description:
+      'Allows advertisers to use a company logo and use a longer presentation text',
+  },
+  premium: {
+    name: 'ad',
+    price: 394.99,
+    description:
+      'Same benefits as Featured Ad, but also puts the advertisement at the top of\n' +
+      'the results, allowing higher visibility',
+  },
 };
