5b2cfd457bd2378602556b627b1f0563f0bedaf6
Farid <fnikman@gmail.com>
2021-06-10 20:10:46 +0800

    fix: remove minHeight

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -1,27 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useStoreRehydrated } from 'easy-peasy';
-import { Container, makeStyles, Typography } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
-const usePageLoaderStyles = makeStyles(() => ({
-  container: {
-    minHeight: '100vh',
-  },
-}));
 export function PageLoader({ children }) {
-  const classes = usePageLoaderStyles();
-
   const isRehydrated = useStoreRehydrated();
 
   if (!isRehydrated) {
     return (
-      <Container className={classes.container}>
+      <Container>
         <Typography>Loading</Typography>
       </Container>
     );
   }
 
-  return <Container className={classes.container}>{children}</Container>;
+  return <Container>{children}</Container>;
 }
 
 PageLoader.propTypes = {
